2d4443f229188ff7d0133b4be54481770a9a3f4e
Franz Noel Tanglao <franz@tanglao.org>
2023-04-18 21:30:55 -0700

    init: Dr. Cris website

---
+++ webpack.config.js
@@ -0,0 +1,128 @@
+import Dotenv from "dotenv-webpack"
+import path from "path"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import CopyPlugin from "copy-webpack-plugin"
+
+let htmlPageNames = ['contact-us', 'about'];
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: `./${name}.html`, // relative path to the HTML files
+    filename: `${name}/index.html`, // output HTML files
+    chunks: [`main`] // respective JS files
+  })
+});
+
+export default {
+  // Define the entry points of our application (can be multiple for different sections of a website)
+  entry: {
+    main: "./src/js/main.js",
+  },
+
+  // Define the destination directory and filenames of compiled resources
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(process.cwd(), "./public"),
+  },
+
+  // Define development options
+  devtool: "source-map",
+
+  // Define loaders
+  module: {
+    rules: [
+      // Use babel for JS files
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ]
+          }
+        }
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              url: false,
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "autoprefixer",
+                ]
+              }
+            }
+          },
+          "sass-loader"
+        ],
+      },
+      // File loader for images
+      {
+        test: /\.(jpg|jpeg|png|git|svg)$/i,
+        type: "asset/resource",
+      }
+    ],
+  },
+
+  // Define used plugins
+  plugins: [
+    // Load .env file for environment variables in JS
+    new Dotenv({
+      path: "./.env"
+    }),
+
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css"
+    }),
+
+    // Copy images to the public folder
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/images",
+          to: "images",
+        }
+      ]
+    }),
+
+    // Inject styles and scripts into the HTML
+    new HtmlWebpackPlugin({
+      template: path.resolve(process.cwd(), "index.html")
+    }),
+  ].concat(multipleHtmlPlugins),
+
+  // Configure the "webpack-dev-server" plugin
+  devServer: {
+    static: {
+      directory: path.resolve(process.cwd(), "public")
+    },
+    watchFiles: [
+      path.resolve(process.cwd(), "index.html"),
+      path.resolve(process.cwd(), "contact-us.html")
+    ],
+    compress: true,
+    port: process.env.PORT || 9090,
+    hot: true,
+  },
+
+  // Performance configuration
+  performance: {
+    hints: false
+  }
+};
